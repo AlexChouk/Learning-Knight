7570df124ebf726aebd725b87634048f1a327a8b
AlexChouk <alexandre.pene.couck@gmail.com>
2023-02-02 10:51:27 +0100

    add player's interactions & combat trigger

--- Assets/Script/FocusCamera.cs
+++ Assets/Script/FocusCamera.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 public class FocusCamera : MonoBehaviour
 {
     [SerializeField] private GameObject knight;
+    public float camSpeed;
+    private Move knightMove;
+
+    private gameManager gm;
+
+    private Camera cam;
+
+    private Vector3 camPos;
+
+    private float camSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(knight.transform.position.x +10, transform.position.y, -10);
+        knightMove = knight.GetComponent<Move>();
+        camSpeed = knightMove.speed;
+        transform.position = new Vector3(knight.transform.position.x + camSpeed + 15, transform.position.y, -15);
+        gm = GameObject.Find("gameManager").GetComponent<gameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(knight.transform.position.x +10, transform.position.y, -10);
+        camSpeed = knightMove.speed;
+        if(! gm.isCurrentlyFighting()){
+            transform.position = new Vector3(transform.position.x + camSpeed, transform.position.y, -15);
+        }
+        else{
+            startFightFocus();
+        }
     }
+
+    void startFightFocus(){
+        FightMode(gm.getHero(),gm.getEnemy());
+    }
+
+
+    void FightMode(GameObject hero, GameObject enemy){
+        cam = gameObject.GetComponent<Camera>();
+        camSize = cam.orthographicSize;
+        camPos = gameObject.transform.position;
+        float x = enemy.transform.position.x - hero.transform.position.x;
+        gameObject.transform.position = new Vector3(hero.transform.position.x + x/2,-21,-11);
+        cam.orthographicSize = 6;
+
+    }
+
+    void endFightMode(){
+        transform.position = camPos;
+        cam.orthographicSize = camSize;
+    }
+
 }
---
+++ Assets/Script/Slide.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slide : MonoBehaviour
+{
+    //public Sprite originalSprite;
+    public bool isSliding;
+    private SpriteRenderer sp;
+    //public Sprite spriteForSlide;
+    private BoxCollider2D boxCollider2D;
+    public bool isUnderBlockBool = false;
+    [SerializeField] public LayerMask groundLayerMask;
+
+    private Jump jump;
+
+    private Move move;
+
+    private gameManager gm;
+
+    public bool slideCompleted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slideCompleted = false;
+        isSliding = false;
+        boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+        sp = gameObject.GetComponent<SpriteRenderer>();
+        jump = gameObject.GetComponent<Jump>();
+        move = gameObject.GetComponent<Move>();
+        gm = GameObject.Find("gameManager").GetComponent<gameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.B) && !isSliding && !gm.isCurrentlyFighting()){
+            if(! jump.isJumping && ! move.isSprinting){
+                Debug.Log("lets start");
+                slideCompleted = false;
+                proceedSlide();
+            }
+        }
+
+        if(isUnderBlockBool && isSliding){
+            Debug.Log("lets continue");
+            StartCoroutine(startSlide());
+        }
+        
+        if(isSliding && !isUnderBlockBool && slideCompleted){
+            Debug.Log("slideFinish");
+            transform.eulerAngles = new Vector3(0,0,0);
+            resetStats();
+        }
+    }
+
+    void resetStats(){
+        isSliding = false;
+        slideCompleted = false;
+        isUnderBlockBool = false;
+    }
+
+    void proceedSlide(){
+        if(! isSliding){
+            isSliding = true;
+            transform.eulerAngles = new Vector3(0,0,90);
+        }
+            StartCoroutine(startSlide());
+    }
+
+    IEnumerator startSlide(){
+        yield return new WaitForSeconds(1f);
+        if(! slideCompleted){ slideCompleted = true; }
+        isUnderBlock();
+    }
+
+    private void isUnderBlock(){
+        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.up, 2f, groundLayerMask);
+        isUnderBlockBool = (raycast.collider != null);
+    }
+}
